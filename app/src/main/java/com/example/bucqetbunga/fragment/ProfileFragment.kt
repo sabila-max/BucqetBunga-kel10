245576a2f73a1b7207873e895585a96178a4dbac
User <abeldewanti2803@gmail.com>
2025-12-02 15:56:16 +0700

    memberikan button logout ke dalam profile

--- app/src/main/java/com/example/bucqetbunga/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/bucqetbunga/fragment/ProfileFragment.kt
@@ -1,11 +1,15 @@
 package com.example.bucqetbunga.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.bucqetbunga.R
+import com.example.bucqetbunga.activities.LoginActivity
+import com.example.bucqetbunga.utils.SessionManager
 
 class ProfileFragment : Fragment() {
 
@@ -15,5 +19,23 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Ambil tombol dari layout
+        val btnLogout = view.findViewById<Button>(R.id.btnLogout)
+
+        btnLogout.setOnClickListener {
+            // Besihkan session
+            val session = SessionManager(requireContext())
+            session.logout()
+
+            // Arahkan balik ke login activity
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            intent.flags =Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
     }
 }
